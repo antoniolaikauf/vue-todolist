1b7a9cdad5d6a11a56fa70809a1fb0cb45d88366
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-12-12 13:15:52 +0100

    add: aggiunto le tasks dentro l'array in html

---
+++ JS/index.js
@@ -0,0 +1,40 @@
+// const todos: [
+//     {
+//         text: 'Fare i compiti',
+//         done: false
+//     },
+//     {
+//         text: 'Fare la spesa',
+//         done: true
+//     },
+//     {
+//         text: 'Fare il bucato',
+//         done: false
+//     }
+// ]
+
+
+
+const {createApp}= Vue;
+
+createApp({
+    data(){
+        return{
+        // array di tasks 
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ],
+        }
+    }
+}).mount("#app")
